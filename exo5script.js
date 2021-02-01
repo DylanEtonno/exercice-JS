d9f7c3c8be017202b603bc228e95b65c78e5f3dc
DylanEtonno <78016445+DylanEtonno@users.noreply.github.com>
2021-02-01 15:54:47 +0100

    Add files via upload

---
+++ exo5script.js
@@ -0,0 +1,15 @@
+function test1() {
+let a=2;
+if (a<10){
+alert('inferieur à 10');}
+else{
+alert('superieur a 10');}}
+test1();
+
+//function test2() {
+for(let i=1; i<11;i++){
+	
+	document.getElementById("p1").innerHTML
++= i;
+}
+//test2();
